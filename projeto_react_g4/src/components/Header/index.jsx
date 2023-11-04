e561d6a1034553da34b412f5deeca25cf7ebbbb4
Thiago Bellato <tfbellato@hotmail.com>
2023-11-04 18:09:39 -0300

    Feat: API Via Cep (Em andamento)

--- projeto_react_g4/src/components/Header/index.jsx
+++ projeto_react_g4/src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import './style.css'
 
 export default function Header() {
@@ -7,34 +7,37 @@ export default function Header() {
     <header>
       {/* <h2>Empresa ABC</h2> */}
       <div className="menu">
-            <div id="tituloHeader">
-              
-              <img src="../../../src/assets/Brasilianas.svg" alt="logo" />
-                </div>
-            <nav>
-              <ul>
-                
-                <li>
-                  <Link to={'/'}><button className="button">Home</button></Link></li>
-                  
-                <li> 
-            <Link to={'/empresa'}><button className="button">Empresa</button></Link>
-                </li>
-                <li>
-            <Link to={'/produto'}><button className="button">Produtos</button></Link>
-                </li>
-                <li>
-            <Link to={'/sobre'}><button className="button"> Sobre</button></Link>
-                </li>
-                <li>
-            <Link to={'/contato'}><button className="button">Contato</button></Link>
-            </li>
-                <li>
-            <Link to={'/calc'}><button className="button">Calculadora</button></Link>
-            </li>
-              </ul>
+        <div id="tituloHeader">
 
-            </nav>
+          <img src="../../../src/assets/Brasilianas.svg" alt="logo" />
+        </div>
+        <nav>
+          <ul>
+
+            <li>
+              <Link to={'/'}><button className="button">Home</button></Link></li>
+
+            <li>
+              <Link to={'/empresa'}><button className="button">Empresa</button></Link>
+            </li>
+            <li>
+              <Link to={'/produto'}><button className="button">Produtos</button></Link>
+            </li>
+            <li>
+              <Link to={'/sobre'}><button className="button"> Sobre</button></Link>
+            </li>
+            <li>
+              <Link to={'/contato'}><button className="button">Contato</button></Link>
+            </li>
+            <li>
+              <Link to={'/calc'}><button className="button">Calculadora</button></Link>
+            </li>
+            <li>
+              <Link to={'/localizacao'}><button className="button">Localização</button></Link>
+            </li>
+          </ul>
+
+        </nav>
       </div>
     </header>
   );
